1b9dee852d1f124de275408328db058e2c63f970
Crystalin <alan@purestake.com>
2022-10-02 23:12:49 -0400

    Fix time promise

--- src/commands/block-time/block-time.ts
+++ src/commands/block-time/block-time.ts
@@ -63,28 +63,30 @@ export class BlockTimeTask extends Task {
       throw new Error("Cannot (yet) compute past date blocks");
     }
     let progress = 0;
-    await this.networkApis.map(async ({ api, name }) => {
-      if (this.cancelled) {
-        throw new Error("Cancelled");
-      }
-      const { block, date, blockCount } = await computeBlockForMoment(
-        api,
-        targetDate
-      );
-      this.emit(
-        "log",
-        "info",
-        `${name.padStart(this.namePadding, " ")}: #${block
-          .toString()
-          .padEnd(8, " ")} (+${blockCount
-          .toString()
-          .padEnd(8, " ")}) - ${date.format("dddd, MMMM Do YYYY, h:mm:ss a")}`
-      );
-      this.emit(
-        "progress",
-        Math.round((progress += 100 / this.networkApis.length))
-      );
-    });
+    await Promise.all(
+      this.networkApis.map(async ({ api, name }) => {
+        if (this.cancelled) {
+          throw new Error("Cancelled");
+        }
+        const { block, date, blockCount } = await computeBlockForMoment(
+          api,
+          targetDate
+        );
+        this.emit(
+          "log",
+          "info",
+          `${name.padStart(this.namePadding, " ")}: #${block
+            .toString()
+            .padEnd(8, " ")} (+${blockCount
+            .toString()
+            .padEnd(8, " ")}) - ${date.format("dddd, MMMM Do YYYY, h:mm:ss a")}`
+        );
+        this.emit(
+          "progress",
+          Math.round((progress += 100 / this.networkApis.length))
+        );
+      })
+    );
   }
   async cancel() {
     this.cancelled = true;
