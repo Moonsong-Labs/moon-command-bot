539f71aca762558bdbee6c5eae40bd491abd7af9
Crystalin <alan@purestake.com>
2022-10-04 11:54:43 -0400

    Better command/hook env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import fs from "node:fs/promises";
-import { promisify } from "node:util";
 import http from "node:http";
 import express from "express";
 import { Commander } from "./commands/commander";
@@ -14,14 +13,14 @@ import { SlackHook } from "./hooks/slack-hook";
 import { TaskFactory } from "./commands/factory";
 import { GithubHook } from "./hooks/github-hook";
 import { BlockTimeFactory } from "./commands/block-time/factory";
-import { getApiFor } from "moonbeam-tools";
 
 let isTerminating = false;
 
 let commander: Commander;
-let hooks: Hook[];
-let factories: TaskFactory[];
-let octoServices: { moonbeamRepo: OctokitService; forkRepo: OctokitService };
+let hooks: Hook[] = [];
+let factories: TaskFactory[] = [];
+let octoServices: { moonbeamRepo?: OctokitService; forkRepo?: OctokitService } =
+  {};
 let server: http.Server;
 
 export async function destroy() {
@@ -66,52 +65,58 @@ export async function main() {
   if (process.env.DEBUG) {
     console.log("Running in debug mode");
   }
-  const moonbeamPrivateKey = (
-    await fs.readFile(process.env.MOONBEAM_PRIVATE_PEM)
-  ).toString();
-  const forkPrivateKey = (
-    await fs.readFile(process.env.FORK_PRIVATE_PEM)
-  ).toString();
-
-  octoServices = {
-    moonbeamRepo: new OctokitService(
-      process.env.MOONBEAM_REPO_OWNER,
-      process.env.MOONBEAM_REPO_NAME,
-      process.env.MOONBEAM_INSTALLATION_ID,
-      {
-        appId: process.env.MOONBEAM_APP_ID,
-        clientId: process.env.MOONBEAM_CLIENT_ID,
-        clientSecret: process.env.MOONBEAM_CLIENT_SECRET,
-        privateKey: moonbeamPrivateKey,
-      }
-    ),
-    forkRepo: new OctokitService(
-      process.env.FORK_REPO_OWNER,
-      process.env.FORK_REPO_NAME,
-      process.env.FORK_INSTALLATION_ID,
-      {
-        appId: process.env.FORK_APP_ID,
-        clientId: process.env.FORK_CLIENT_ID,
-        clientSecret: process.env.FORK_CLIENT_SECRET,
-        privateKey: forkPrivateKey,
-      }
-    ),
-  };
 
   const app = express();
   server = new http.Server(app);
 
-  factories = [
-    new SampleFactory("sample"),
-    new BenchmarkFactory("benchmark", octoServices),
-    new BlockTimeFactory("block-time", ["alphanet", "moonriver", "moonbeam"]),
-  ];
+  factories.push(new SampleFactory("sample"));
+  console.log(`- Enable command: sample`);
+  factories.push(
+    new BlockTimeFactory("block-time", ["alphanet", "moonriver", "moonbeam"])
+  );
+  console.log(`- Enable command: block-time`);
+
+  if (process.env.BENCHMARK_COMMAND) {
+    const moonbeamPrivateKey = (
+      await fs.readFile(process.env.MOONBEAM_PRIVATE_PEM)
+    ).toString();
+    const forkPrivateKey = (
+      await fs.readFile(process.env.FORK_PRIVATE_PEM)
+    ).toString();
+
+    octoServices = {
+      moonbeamRepo: new OctokitService(
+        process.env.MOONBEAM_REPO_OWNER,
+        process.env.MOONBEAM_REPO_NAME,
+        process.env.MOONBEAM_INSTALLATION_ID,
+        {
+          appId: process.env.MOONBEAM_APP_ID,
+          clientId: process.env.MOONBEAM_CLIENT_ID,
+          clientSecret: process.env.MOONBEAM_CLIENT_SECRET,
+          privateKey: moonbeamPrivateKey,
+        }
+      ),
+      forkRepo: new OctokitService(
+        process.env.FORK_REPO_OWNER,
+        process.env.FORK_REPO_NAME,
+        process.env.FORK_INSTALLATION_ID,
+        {
+          appId: process.env.FORK_APP_ID,
+          clientId: process.env.FORK_CLIENT_ID,
+          clientSecret: process.env.FORK_CLIENT_SECRET,
+          privateKey: forkPrivateKey,
+        }
+      ),
+    };
+    new BenchmarkFactory("benchmark", octoServices as any);
+    console.log(`- Enable command: benchmark`);
+  }
 
   const taskHistory = new TaskHistory(1000);
 
   const hooks: Hook[] = [new HttpHook({ express: app })];
-  if (process.env.SLACK_APP_TOKEN) {
-    console.log(`- Enable Slack hook`);
+  if (process.env.SLACK_HOOK) {
+    console.log(`- Enable hook: Slack `);
     hooks.push(
       new SlackHook({
         appToken: process.env.SLACK_APP_TOKEN,
@@ -121,15 +126,30 @@ export async function main() {
       })
     );
   }
-  if (process.env.GITHUB_WEBHOOK_SECRET) {
-    console.log(`- Enable Github hook`);
+  if (process.env.GITHUB_HOOK) {
+    console.log(`- Enable hook: Github`);
+    const githubPrivateKey = (
+      await fs.readFile(process.env.GITHUB_PRIVATE_PEM)
+    ).toString();
     hooks.push(
       new GithubHook({
         webhookSecret: process.env.GITHUB_WEBHOOK_SECRET,
-        privateKey: moonbeamPrivateKey,
-        appId: process.env.MOONBEAM_APP_ID,
+        privateKey: githubPrivateKey,
+        appId: process.env.GITHUB_APP_ID,
         express: app,
-        octoRepos: Object.values(octoServices),
+        octoRepos: [
+          new OctokitService(
+            process.env.GITHUB_REPO_OWNER,
+            process.env.GITHUB_REPO_NAME,
+            process.env.GITHUB_INSTALLATION_ID,
+            {
+              appId: process.env.GITHUB_APP_ID,
+              clientId: process.env.GITHUB_CLIENT_ID,
+              clientSecret: process.env.GITHUB_CLIENT_SECRET,
+              privateKey: githubPrivateKey,
+            }
+          ),
+        ],
       })
     );
   }
