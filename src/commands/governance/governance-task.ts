311b4feb762d223a1fed28291e6fe6ef812beaca
Crystalin <alan.sapede@gmail.com>
2022-12-27 11:47:17 +0100

    Adds support for OpenGov

--- src/commands/governance/governance-task.ts
+++ src/commands/governance/governance-task.ts
@@ -1,12 +1,14 @@
 import { BN, BN_TEN } from "@polkadot/util";
 import { ApiPromise } from "@polkadot/api";
+
 import { Task } from "../task";
 import { moment } from "moment-parseplus";
 import humanizeNumber from "humanize-number";
 
+import { callInterpreter, getReferendumByGroups } from "moonbeam-tools";
 import Debug from "debug";
 import { getBlockDate } from "../../actions/block-time";
-import { callInterpreter, renderCallMarkdown } from "../../utils/call";
+import { renderCallMarkdown } from "../../utils/call";
 const debug = Debug("commands:Governance");
 
 export interface Network {
@@ -37,32 +39,29 @@ export class GovernanceTask extends Task {
   public async execute() {
     //
     let progress = 0;
-    const networkReferendums = await Promise.all(
+    const networkDemocracy = await Promise.all(
       this.parameters.networkApis.map(async ({ api, name }) => {
         const header = await api.rpc.chain.getHeader();
         const blockNumber = header.number.toNumber();
         const blockHash = header.hash.toString();
 
-        const referendums = await api.derive.democracy.referendums();
+        const referendumV1 = await api.derive.democracy.referendums();
         const messages = await Promise.all(
-          referendums.map(async (referendum) => {
+          referendumV1.map(async (referendum) => {
             const preimageHash = referendum.imageHash;
             const polkadotPrefix = name == "Moonbase Alpha" ? "moonbase" : name;
 
-            const enactBlock = referendum.status.end
-              .add(referendum.status.delay)
-              .toNumber();
             const endBlock = referendum.status.end.isubn(1).toNumber();
-            const preimage = await api.query.democracy.preimages(preimageHash);
-            let imageText = ""; // TODO refactor
-            let subText = null; // TODO refactor
-            if (preimage && preimage.isSome && preimage.unwrap().isAvailable) {
+            let imageText = "";
+            let subText = null;
+            if (
+              referendum.image &&
+              referendum.image.proposal &&
+              referendum.image.proposal
+            ) {
               const callData = await callInterpreter(
                 api,
-                await api.registry.createType(
-                  "Call",
-                  preimage.unwrap().asAvailable.data.toU8a(true)
-                )
+                await api.registry.createType("Call", referendum.image.proposal)
               );
               imageText = callData.text;
               subText =
@@ -84,7 +83,7 @@ export class GovernanceTask extends Task {
             );
 
             return {
-              end: endBlock,
+              sortKey: endBlock,
               message: `${
                 referendum.isPassing ? `:green_circle:` : `:red_circle:`
               } [${referendum.index
@@ -106,14 +105,269 @@ export class GovernanceTask extends Task {
             };
           })
         );
+
         this.emit(
           "progress",
           { time: Date.now() },
           Math.round((progress += 100 / this.parameters.networkApis.length))
         );
-        return { name, messages };
+        return { version: "1", name, messages };
       })
     );
+
+    const networkReferendums = await Promise.all(
+      this.parameters.networkApis.map(async ({ api, name }) => {
+        const totalIssuance = await api.query.balances.totalIssuance();
+        const currentBlock = (
+          await api.rpc.chain.getHeader()
+        ).number.toNumber();
+
+        const toBlockMoment = async (
+          api: ApiPromise,
+          endBlock: BN,
+          symbol: string
+        ) => {
+          if (currentBlock > endBlock.toNumber()) {
+            return `${moment
+              .duration(
+                moment(
+                  (await getBlockDate(api, endBlock.toNumber())).date
+                ).diff(moment())
+              )
+              .humanize()} ago`;
+          }
+          return `${moment
+            .duration(
+              moment((await getBlockDate(api, endBlock.toNumber())).date).diff(
+                moment()
+              )
+            )
+            .humanize()}->${symbol}`;
+        };
+
+        const header = await api.rpc.chain.getHeader();
+        const blockNumber = header.number.toNumber();
+        const blockHash = header.hash.toString();
+
+        const referendum = await getReferendumByGroups(api);
+        const messages = await Promise.all(
+          referendum.map(async (ref) => {
+            const polkadotPrefix = name == "Moonbase Alpha" ? "moonbase" : name;
+
+            const enactmentDelayFromNow = ref.ongoing.enactment.isAfter
+              ? currentBlock + ref.ongoing.enactment.asAfter.toNumber()
+              : ref.ongoing.enactment.asAt.toNumber();
+
+            const isExecuted =
+              ref.info.isApproved &&
+              ((ref.ongoing.enactment.isAfter &&
+                ref.info.asApproved[0]
+                  .add(ref.ongoing.enactment.asAfter)
+                  .toNumber() < currentBlock) ||
+                (ref.ongoing.enactment.isAt &&
+                  ref.ongoing.enactment.asAt.toNumber() < currentBlock));
+
+            const networkIcon =
+              polkadotPrefix == "moonbeam"
+                ? "🌒"
+                : polkadotPrefix == "moonriver"
+                ? "⛵"
+                : "?";
+            const statusIcon = ref.info.isApproved
+              ? isExecuted
+                ? "⚡"
+                : "✅"
+              : ref.info.isCancelled
+              ? "⛔"
+              : ref.info.isKilled
+              ? "💀"
+              : ref.info.isRejected
+              ? "🟥"
+              : ref.info.isTimedOut
+              ? "🕑"
+              : ref.info.isOngoing
+              ? ref.info.asOngoing.deciding.isSome &&
+                ref.info.asOngoing.deciding.unwrap().confirming.isSome
+                ? "❗"
+                : "📰"
+              : "?";
+
+            const callData =
+              ref.image && (await callInterpreter(api, ref.image.proposal));
+            const imageText =
+              callData && callData.text
+                ? callData.text.startsWith("whitelist.dispatch")
+                  ? `${
+                      ref.info.isOngoing
+                        ? (
+                            await api.query.whitelist.whitelistedCall(
+                              callData.subCalls[0].call.hash.toHex()
+                            )
+                          ).isSome
+                          ? `🔓`
+                          : `🔐`
+                        : ""
+                    }[${callData.subCalls[0].text}]`
+                  : callData.text
+                : "";
+            const subText =
+              !callData ||
+              callData.depth == 0 ||
+              callData.text.startsWith("whitelist.dispatch")
+                ? null
+                : callData.subCalls
+                    .map((c) => renderCallMarkdown(c, "  \n", 1))
+                    .join("  \n");
+
+            const yes = ref.ongoing.tally.ayes.div(
+              BN_TEN.pow(new BN(api.registry.chainDecimals[0]))
+            );
+
+            const no = ref.ongoing.tally.nays.div(
+              BN_TEN.pow(new BN(api.registry.chainDecimals[0]))
+            );
+
+            const supportPercent =
+              ref.ongoing.tally.support
+                .muln(10_000)
+                .div(totalIssuance)
+                .toNumber() / 100;
+
+            const nextStepTime = ref.info.isApproved
+              ? ref.ongoing.enactment.isAfter
+                ? `${await toBlockMoment(
+                    api,
+                    ref.info.asApproved[0].add(ref.ongoing.enactment.asAfter),
+                    "⚡"
+                  )}`
+                : `${await toBlockMoment(
+                    api,
+                    ref.ongoing.enactment.asAt,
+                    "⚡"
+                  )}`
+              : ref.info.isOngoing
+              ? ref.info.asOngoing.deciding.isSome
+                ? ref.info.asOngoing.deciding.unwrap().confirming.isSome
+                  ? `${await toBlockMoment(
+                      api,
+                      ref.track.confirmPeriod.add(
+                        ref.info.asOngoing.deciding.unwrap().confirming.unwrap()
+                      ),
+                      "✅"
+                    )}`
+                  : ref.decidingEnd
+                  ? `${await toBlockMoment(api, ref.decidingEnd, "❗")}`
+                  : `${await toBlockMoment(
+                      api,
+                      ref.track.preparePeriod
+                        .add(ref.info.asOngoing.submitted)
+                        .add(ref.track.decisionPeriod),
+                      "⏱"
+                    )}`
+                : `${await toBlockMoment(
+                    api,
+                    ref.track.preparePeriod
+                      .add(ref.info.asOngoing.submitted)
+                      .add(ref.track.decisionPeriod),
+                    "⏱"
+                  )}`
+              : "";
+
+            const additionalConfirmingTime =
+              ref.info.isOngoing &&
+              (ref.info.asOngoing.deciding.isNone ||
+                ref.info.asOngoing.deciding.unwrap().confirming.isNone)
+                ? `+${moment
+                    .duration(
+                      moment(
+                        (
+                          await getBlockDate(
+                            api,
+                            ref.track.confirmPeriod
+                              .addn(currentBlock)
+                              .toNumber()
+                          )
+                        ).date
+                      ).diff(moment())
+                    )
+                    .humanize()}->✅`
+                : null;
+
+            const additionalEnactmentTime =
+              ref.info.isOngoing &&
+              (ref.info.asOngoing.deciding.isNone ||
+                ref.info.asOngoing.deciding.unwrap().confirming.isNone)
+                ? `+${moment
+                    .duration(
+                      moment(
+                        (await getBlockDate(api, enactmentDelayFromNow)).date
+                      ).diff(moment())
+                    )
+                    .humanize()}->⚡`
+                : null;
+
+            return {
+              sortKey:
+                (ref.info.isApproved
+                  ? ref.info.asApproved[0]
+                  : ref.info.isCancelled
+                  ? ref.info.asCancelled[0]
+                  : ref.info.isKilled
+                  ? ref.info.asKilled[0]
+                  : ref.info.isRejected
+                  ? ref.info.asRejected[0]
+                  : ref.info.isTimedOut
+                  ? ref.info.asTimedOut[0]
+                  : ref.info.isOngoing
+                  ? ref.info.asOngoing.deciding.isSome &&
+                    ref.info.asOngoing.deciding.unwrap().confirming.isSome
+                    ? ref.track.confirmPeriod.add(
+                        ref.info.asOngoing.deciding.unwrap().confirming.unwrap()
+                      )
+                    : ref.decidingEnd
+                  : ref.decidingEnd
+                )?.toBigInt?.() || 0n,
+              message:
+                `${networkIcon}` +
+                `[${ref.track.name
+                  .toString()
+                  .slice(0, 15)
+                  .padStart(15, " ")}]` +
+                `${ref.id.toString().padStart(3, " ")} -` +
+                `${imageText.slice(0, 40).padStart(40, " ")}` +
+                `${statusIcon}` +
+                ` |👍${humanizeNumber(yes.toNumber()).padStart(
+                  10,
+                  " "
+                )} vs ${humanizeNumber(no.toNumber()).padStart(10, " ")}👎` +
+                `|${supportPercent.toFixed(2).padStart(5, " ")}%` +
+                (nextStepTime
+                  ? `|${nextStepTime[isExecuted ? "padStart" : "padStart"](
+                      15,
+                      " "
+                    )}`
+                  : "") +
+                (additionalConfirmingTime
+                  ? `|${additionalConfirmingTime.padStart(15, " ")}`
+                  : "") +
+                (additionalEnactmentTime
+                  ? `|${additionalEnactmentTime.padStart(15, " ")}`
+                  : "") +
+                `|` +
+                (subText ? `\n                 ${subText}` : ""),
+            };
+          })
+        );
+
+        this.emit(
+          "progress",
+          { time: Date.now() },
+          Math.round((progress += 100 / this.parameters.networkApis.length))
+        );
+        return { version: "1", name, messages };
+      })
+    );
+
     this.emit(
       "result",
       { time: Date.now() },
@@ -122,7 +376,7 @@ export class GovernanceTask extends Task {
         .map(
           ({ name, messages }) =>
             `#### ${name}  \n${messages
-              .sort((a, b) => a.end - b.end)
+              .sort((a, b) => Number(a.sortKey) - Number(b.sortKey))
               .map(({ message }) => message)
               .join("  \n")}`
         )
--- src/utils/call.ts
+++ src/utils/call.ts
@@ -1,23 +1,4 @@
-import { ApiPromise } from "@polkadot/api";
-import { GenericCall } from "@polkadot/types/generic";
-
-const NESTED_CALLS = [
-  {
-    section: "utility",
-    method: "dispatchAs",
-    multi: false,
-    argumentPosition: 1,
-  },
-  { section: "sudo", method: "sudo", multi: false, argumentPosition: 0 },
-  { section: "sudo", method: "sudoAs", multi: false, argumentPosition: 1 },
-  { section: "batch", method: "batch", multi: true, argumentPosition: 0 },
-];
-
-export interface CallInterpretation {
-  text: string;
-  depth: number;
-  subCalls: CallInterpretation[];
-}
+import { CallInterpretation } from "moonbeam-tools";
 
 export function renderCallMarkdown(
   callData: CallInterpretation,
@@ -30,42 +11,4 @@ export function renderCallMarkdown(
       renderCallMarkdown(call, separator, depth + 1)
     ),
   ].join("\n");
-}
-
-export async function callInterpreter(
-  api: ApiPromise,
-  call: GenericCall
-): Promise<CallInterpretation> {
-  const nested = NESTED_CALLS.find(
-    ({ section, method }) =>
-      section == call.section.toString() && method == call.method.toString()
-  );
-  const text = `${call.section}.${call.method}`;
-  if (nested) {
-    if (nested.multi) {
-      const subCalls = await api.registry.createType(
-        "Vec<Call>",
-        call.args[nested.argumentPosition].toU8a(true)
-      );
-      const subCallsData = await Promise.all(
-        subCalls.map((subCall) => callInterpreter(api, subCall))
-      );
-      return {
-        text,
-        depth:
-          subCallsData.length > 0
-            ? Math.max(...subCallsData.map((sub) => sub.depth)) + 1
-            : 1,
-        subCalls: subCallsData,
-      };
-    }
-    const subCall = await api.registry.createType(
-      "Call",
-      call.args[nested.argumentPosition].toU8a(true)
-    );
-
-    return { text, depth: 1, subCalls: [await callInterpreter(api, subCall)] };
-  }
-
-  return { text: `${call.section}.${call.method}`, depth: 0, subCalls: [] };
 }
